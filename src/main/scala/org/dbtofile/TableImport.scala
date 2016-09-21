ca0d2638fcf86217536e0b9c19c0c9288d6f1c66
Oleh Hrynets <Oleh_Hrynets@epam.com>
2016-09-21 15:36:32 -0400

    Added CLI parameters to:         generate - to generate YAM from database         load - to load data from database         merge - to merge base table with child tables

--- src/main/scala/org/dbtofile/TableImport.scala
+++ src/main/scala/org/dbtofile/TableImport.scala
@@ -20,9 +20,10 @@ package org.dbtofile
 import java.io.{File, FileInputStream}
 
 import org.apache.spark.sql.{SaveMode, SparkSession}
-import org.dbtofile.conf.TableList
+import org.dbtofile.conf.{DataSourceDefinition, TableList}
 import org.dbtofile.load.DataLoader
 import org.dbtofile.merge.DataSourceMerger
+import org.dbtofile.utils.YamlOps
 import org.yaml.snakeyaml.Yaml
 import org.yaml.snakeyaml.constructor.Constructor
 
@@ -32,44 +33,93 @@ import org.yaml.snakeyaml.constructor.Constructor
 object TableImport {
 
   def main(args: Array[String]): Unit = {
-    case class Config( conf: File = new File("."), loadTables: Boolean = false)
+    case class Config( conf: File = new File("."), loadTables: Boolean = false,
+                       generateConf: Boolean = false,
+                       mergeTables: Boolean = false,
+                       dbUrl: Option[String] = Option.empty[String],
+                       dbUsername: Option[String] = Option.empty[String],
+                       dbPassword: Option[String] = Option.empty[String],
+                       dbConfOutput: Option[File] = Option.empty[File])
     val parser = new scopt.OptionParser[Config]("scopt") {
       head("TableImport", "0.1")
 
-      opt[File]('c', "conf").required().valueName("").
-        action( (x, c) => c.copy(conf = x) ).
-        text("conf is a required configuration property")
+
+      cmd("load").action( (_, c) => c.copy(loadTables = true) ).
+        text("load command.").
+        children(
+          opt[File]('c', "conf").required().valueName("").
+            action( (x, c) => c.copy(conf = x) ).
+            text("conf is a required configuration property")
+        )
+      cmd("merge").action( (_, c) => c.copy(mergeTables = true) ).
+        text("merge command.").
+        children(
+          opt[File]('c', "conf").required().valueName("").
+            action( (x, c) => c.copy(conf = x) ).
+            text("conf is a required configuration property")
+        )
+      cmd("generate").action( (_, c) => c.copy(generateConf = true) ).
+        text("generate command.").
+        children(
+          opt[String]('d',"db-url").required().action( (x, c) =>
+            c.copy(dbUrl = Option(x)) ).text("External Database URL"),
+          opt[String]('u',"db-username").required().action( (x, c) =>
+            c.copy(dbUsername = Option(x)) ).text("External Database Username"),
+          opt[String]('p',"db-password").required().action( (x, c) =>
+            c.copy(dbPassword = Option(x)) ).text("External Database Password"),
+          opt[String]('f',"output-file").action( (x, c) =>
+            c.copy(dbConfOutput = Option(new File(x))) ).text("Output file for generated file")
+        )
     }
 
     parser.parse(args, Config()) match {
       case Some(config) =>
-      // do stuff
-
-        //val conf = new SparkConf
-
-        //val sc = new SparkContext("local[*]", "TableImport", conf)
-        val spark = SparkSession
-          .builder()
-          .master("local[*]")
-          .appName("TableImport")
-          .getOrCreate()
+          if (config.loadTables) {
+            val spark = SparkSession
+              .builder()
+              .master("local[*]")
+              .appName("TableImport")
+              .getOrCreate()
 
 
-//        val filename = "src/main/resources/tables_conf.yaml"
-        val input = new FileInputStream(config.conf)
-        val yaml = new Yaml(new Constructor(classOf[TableList]))
-        val t = yaml.load(input).asInstanceOf[TableList]
+            //        val filename = "src/main/resources/tables_conf.yaml"
+            val input = new FileInputStream(config.conf)
+            val yaml = new Yaml(new Constructor(classOf[TableList]))
+            val t = yaml.load(input).asInstanceOf[TableList]
 
-        for (table <- t.tables) {
-          DataLoader.loadDataFromMySQL(table, spark)
-        }
+            for (table <- t.tables) {
+              DataLoader.loadDataFromMySQL(table, spark)
+            }
+          }
+          if (config.mergeTables) {
+            val spark = SparkSession
+              .builder()
+              .master("local[*]")
+              .appName("TableImport")
+              .getOrCreate()
 
+            //        val filename = "src/main/resources/tables_conf.yaml"
+            val input = new FileInputStream(config.conf)
+            val yaml = new Yaml(new Constructor(classOf[TableList]))
+            val t = yaml.load(input).asInstanceOf[TableList]
 
-        for (merge <- t.merges) {
-          var df = DataSourceMerger.mergeTable(merge, spark)
-          //df.head(10)
-          df.write.mode(SaveMode.Overwrite).format(merge.outputTable.outputFormat).save(merge.outputTable.outputPath)
-        }
+            for (merge <- t.merges) {
+              var df = DataSourceMerger.mergeTable(merge, spark)
+              df.write.mode(SaveMode.Overwrite).format(merge.outputTable.outputFormat).save(merge.outputTable.outputPath)
+            }
+          }
+          if (config.generateConf) {
+            Option.empty.isDefined
+            if (config.dbConfOutput.isDefined) {
+              YamlOps.toString(
+                        DataSourceDefinition.load(config.dbUrl.get, config.dbUsername.get, config.dbPassword.get),
+                        config.dbConfOutput.get)
+            } else {
+              println(YamlOps.toString(
+                DataSourceDefinition.load(config.dbUrl.get, config.dbUsername.get, config.dbPassword.get)))
+            }
+
+          }
 
       case None =>
       // arguments are bad, error message will have been displayed
