76e3d34f0738fd4b7cb7c5482bf6760aa84d6c8c
Ruslan Mendybayev <ruslan_mendybayev@epam.com>
2018-11-29 17:26:07 -0500

    Fixed jar hell Edited run-app.sh

--- run-app.sh
+++ run-app.sh
@@ -4,5 +4,5 @@ spark-submit \
     --class org.dbtofile.TableImport \
     --files conf/app.conf \
     --driver-java-options -Dconfig.file=conf/app.conf \
-    --driver-class-path target/dbtofile-1.0-SNAPSHOT.jar \
-    target/dbtofile-1.0-SNAPSHOT.jar
+    --driver-class-path target/dbtofile-1.2-SNAPSHOT.jar \
+    target/dbtofile-1.2-SNAPSHOT.jar
